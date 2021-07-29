f46917aa380ddccabf40bf759d2e73a8258bd52f
acoderly <47823915+acoderly@users.noreply.github.com>
2021-07-29 09:51:58 +0800

    Update elf_check.py

--- elf/elf_check.py
+++ elf/elf_check.py
@@ -59,6 +59,69 @@ def check_elf_by_ida_script(file):
     IDA_EXE_INSTALLED_PATH = "/Applications/IDAPro7.0/ida.app/Contents/MacOS/ida"
     IDA_PYTHON_SCRIPT = os.path.join(os.path.dirname(__file__), "ida_python_script.py")
     run_ida_script(IDA_EXE_INSTALLED_PATH, IDA_PYTHON_SCRIPT, file)
+    
+    
+def check_elf_by_capstone(file_name):
+    def _get_section_header_stringtable(ELFFile_Obj):
+        return None
+    try:
+        file_obj = open(file_name, "rb")
+    except Exception as e:
+        print(f"open file error {file_name}")
+        return False
+    orgi_get_section_header_stringtable = getattr(ELFFile, "_get_section_header_stringtable", None)
+    setattr(ELFFile, "_get_section_header_stringtable", _get_section_header_stringtable)
+    try:
+        elf_file = ELFFile(file_obj)
+    except Exception as e:
+        print(f"create ELFFile object error. {file_name} {e}")
+        file_obj.close()
+        return False
+    arch = elf_file.get_machine_arch().lower()
+    if arch == "mips" and elf_file.elfclass == 64:
+        arch = "mips64"
+    cs_arch_mode_sm_dict = {
+        "x86": (CS_ARCH_X86, CS_MODE_32),
+        "x64": (CS_ARCH_X86, CS_MODE_64),
+        "mips": (CS_ARCH_MIPS, CS_MODE_MIPS32),
+        "mips64": (CS_ARCH_MIPS, CS_MODE_MIPS64),
+        "arm": (CS_ARCH_ARM, CS_MODE_ARM),
+        "aarch64": (CS_ARCH_ARM64, CS_MODE_ARM),
+    }
+    
+    segment, data = None, None
+    for idx in range(0, ELFFile.num_segments(elf_file)):
+        segment = elf_file.get_segment(idx)
+        if segment.header.p_type != "PT_LOAD":
+            continue
+        if segment.header.p_vaddr < elf_file.header.e_entry < (segment.header.p_vaddr + segment.header.p_memsz):
+            data = segment.data()
+            break
+    if data is None:
+        file_obj.close()
+        return False
+    offset = elf_file.header.e_entry - segment.header.p_vaddr
+    if offset <= 0:
+        file_obj.close()
+        return False
+    # TODO Ugly Magic number.
+    MAGNUM = 10
+    length = MAGNUM * 4
+    item = cs_arch_mode_sm_dict.get(arch, None)
+    if item is None:
+        file_obj.close()
+        return False
+    cs_arch, cs_mode = item
+    cs_mode_endian = CS_MODE_LITTLE_ENDIAN if elf_file.little_endian is True else CS_MODE_BIG_ENDIAN
+    md = Cs(cs_arch, cs_mode + cs_mode_endian)
+    lst = [_ for _ in md.disasm(data[offset:offset + length], elf_file.header.e_entry)]
+    setattr(ELFFile, "_get_section_header_stringtable", orgi_get_section_header_stringtable)
+    file_obj.close()
+    # TODO Ugly code.
+    if arch == "mips":
+        if len(lst) < MAGNUM:
+            return True
+    return False
 
 
 # Test
@@ -71,3 +134,8 @@ for file in os.listdir(DATA_DIR):
 for file in os.listdir(DATA_DIR):
     file_abs_path = os.path.join(DATA_DIR, file)
     check_elf_by_ida_script(file_abs_path)
+    
+# Test
+for file in os.listdir(DATA_DIR):
+    file_abs_path = os.path.join(DATA_DIR, file)
+    check_elf_by_capstone(file_abs_path)
